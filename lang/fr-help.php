ba588dca643efd65fb816d53c27a95db466b2da2
gcyrillus <1832331+gcyrillus@users.noreply.github.com>
2024-01-03 18:16:11 +0100

    Update fr-help.php

--- lang/fr-help.php
+++ lang/fr-help.php
@@ -1,8 +1,15 @@
 <div>
 	<h1>Aide du plugin tell2MyFriend</h1>
 	<p>Un bouton <mark>Recommander à un ami </mark> peut-etre afficher dans vos pages à l'endroit où vous le souhaitez</p>
-	<p>Pour cela, il faut ajouter le Hook du widget , dans votre thème</p>
+	<p>Pour cela, il faut ajouter le Hook du widget</p>
+	<h2>Dans les fichiers du thème</h2>
 	<p>Le code suivant est à copier coller : 
-	<mark style="color:purple;font-weight:bold;padding:0.5em;display:inline-block;vertical-align:middle;"><code>&lt;?php eval($plxShow->callHook('tell2MyFriendwidget')); ?&gt;</code></mark> dans l'un des fichier de votre thème (ou dans une page statique) à l'endroit où vous voulez qu'il apparaisse.
+	<mark style="color:purple;font-weight:bold;padding:0.5em;display:inline-block;vertical-align:middle;"><code>&lt;?php eval($plxShow->callHook('tell2MyFriendwidget')); ?&gt;</code></mark>
+	dans l'un des fichier de votre thème à l'endroit où vous voulez qu'il apparaisse.
+	</p>
+	<h2>Dans une page statique</h2>
+	<p>Le code suivant est à copier coller : 
+	<mark style="color:purple;font-weight:bold;padding:0.5em;display:inline-block;vertical-align:middle;"><code>&lt;?php eval($this->callHook('tell2MyFriendwidget')); ?&gt;</code></mark>
+	dans une page statique en l'éditant à l'endroit où vous voulez qu'il apparaisse.
 	</p>
 </div>
