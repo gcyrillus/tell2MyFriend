b90ebbd2aca8cd545b3c5c0bc1bcc4c58a32ecc2
gcyrillus <1832331+gcyrillus@users.noreply.github.com>
2024-01-04 14:00:53 +0100

    Update site.js
    intercompatibilité avec de potentiels autre antispam natif generés sur la page courante

--- js/site.js
+++ js/site.js
@@ -4,4 +4,15 @@ window.addEventListener("load", (event) => {
 	let template = document.getElementById("myform");
 	let templateContent = template.content;
 	MyTell2Friend.appendChild(templateContent);
+	
+	/*
+	  **** compatibilité avec les antispam des    *****
+	  **** commentaires ou formulaires de contact *****
+	*/
+	// on prend le premier token trouvé
+	let token = document.querySelector('[name="capcha_token"]').value;
+	// on les cherche tous
+	let tokens = document.querySelectorAll('[name="capcha_token"]');
+	// on leur donne tous la même valeur
+	[...tokens].forEach((el) => (el.value = token));
 });
